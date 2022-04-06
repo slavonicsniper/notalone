2784bc60425f456662b8d4be105b3225bb961a68
Erik Galajda <e.galajda@comforte.com>
2022-04-06 19:07:50 +0200

    fix check user confirmation_status

--- services/auth.js
+++ services/auth.js
@@ -32,12 +32,13 @@ const login = async (data) => {
         email
       }
     })
-    if (user.confirmation_status != "Active") {
-      throw createError(401, "Pending Account. Please Verify Your Email!")
-    }
+
     if(!user) {
       throw createError(401, 'Incorrect email or password')
     } else {
+      if (user.confirmation_status != "Active") {
+        throw createError(401, "Pending Account. Please Verify Your Email!")
+      }
       if(await user.validPassword(password)) {
         return user
       } else {
