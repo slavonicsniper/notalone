bfe70e3d6a30aec7b4212ec352cf93412d286680
Erik Galajda <e.galajda@comforte.com>
2022-04-13 16:37:46 +0200

    remove unused imports

--- client/src/components/Activities/Activities.jsx
+++ client/src/components/Activities/Activities.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {Container, Stack} from 'react-bootstrap'
 import AddActivityForm from './AddActivityForm';
 
--- client/src/components/Availability/Availabilities.jsx
+++ client/src/components/Availability/Availabilities.jsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {Container, Stack} from 'react-bootstrap'
 import AddAvailabilityForm from './AddAvailabilityForm';
-import Availability from '../../services/Availability';
 
 export default function Availabilities() {
     return (
