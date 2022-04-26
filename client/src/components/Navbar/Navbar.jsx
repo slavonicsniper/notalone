810f80f78597b1f5cc2a23e5a24026b165046ad8
Weepy <imbrued.mitre@gmail.com>
2022-04-26 18:42:08 +0800

    improved navbar linking, added home button

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import logo from './connect-logo.png';
-import {Container, Navbar, Nav} from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 function Navigation(props) {
 
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
       <Container fluid="xxl">
-        <Navbar.Brand href="/">
+        <Navbar.Brand as={Link} to="/">
           <img
             alt=""
             src={logo}
@@ -15,35 +16,36 @@ function Navigation(props) {
             height="30"
             className="d-inline-block align-top"
           />{' '}
-        NOTAlone
+          NOTAlone
         </Navbar.Brand>
         <>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             {props.loggedIn ?
-            <>
-              <Nav className="me-auto">
-                <Nav.Link href="/inbox">Inbox</Nav.Link>
-                <Nav.Link href="/activities">Activities</Nav.Link>
-                <Nav.Link href="/availabilities">Availabilities</Nav.Link>
-              </Nav>
-              <Nav>
-                <Nav.Link href="/profile">{props.userData.username}</Nav.Link>
-                <Nav.Link href="/logout">Log out</Nav.Link>
-              </Nav>
+              <>
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/inbox">Inbox</Nav.Link>
+                  <Nav.Link as={Link} to="/activities">Activities</Nav.Link>
+                  <Nav.Link as={Link} to="/availabilities">Availabilities</Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link as={Link} to="/profile">{props.userData.username}</Nav.Link>
+                  <Nav.Link as={Link} to="/logout">Log out</Nav.Link>
+                </Nav>
               </>
               :
               <>
-              <Nav className="me-auto">
-              </Nav>
-              <Nav>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/register">Register</Nav.Link>
-              </Nav>
+                <Nav className="me-auto">
+                </Nav>
+                <Nav>
+                  <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                  <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                </Nav>
               </>
             }
           </Navbar.Collapse>
-        </>       
+        </>
       </Container>
     </Navbar>
   )
